1eca45e755f5c9cbfe629e6e1ca845190bc226e7
Mohd Faizan <kfaizan0496@gmail.com>
2023-08-12 00:09:09 +0530

    debug

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 // setting up the mongodb connection through mongoose
 
 const mongoose = require('mongoose');
-const url = 'mongodb://0.0.0.0:27017/db';
+// const url = 'mongodb://0.0.0.0:27017/db';
+const url = 'mongodb://127.0.0.1:27017/db';
+
 
 // mongoose.connect('url') 
 mongoose.connect(url);
