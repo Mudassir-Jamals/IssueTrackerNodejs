b390f0ff9f455f8e316363f648219a6a426876a2
Mohd Faizan <kfaizan0496@gmail.com>
2023-08-13 23:52:36 +0530

    mongoose server timedout

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,14 @@
 // setting up the mongodb connection through mongoose
 
 const mongoose = require('mongoose');
-// const url = 'mongodb://0.0.0.0:27017/db';
+// // const url = 'mongodb://0.0.0.0:27017/db';
 const url = 'mongodb://127.0.0.1:27017/db';
 
 
 // mongoose.connect('url') 
-mongoose.connect(url);
+mongoose.connect(url,{
+    serverSelectionTimeoutMS: 10000,
+});
 const db=mongoose.connection;
 
 //  catch an error when connecting mongodb
@@ -20,3 +22,25 @@ db.once('open',()=>{
 
 // exports this module
 module.exports=db;  
+
+
+
+// const mongoose = require('mongoose');
+// const retry = require('mongoose-retry');
+
+// const connectWithRetry = () => {
+//   return mongoose.connect(url, { useNewUrlParser: true })
+//     .catch(() => retry(connectWithRetry));
+// };
+
+// connectWithRetry();
+
+// mongoose.connect(url, {
+//   useNewUrlParser: true,
+//   serverSelectionTimeoutMS: 10000, // 30 seconds
+// });
+
+
+
+
+
